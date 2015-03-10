20decc30dd1b413e4ee2577106e5f4f799a6ef83
HawkOwl <hawkowl@atleastfornow.net>
2015-03-10 15:46:48 +0800

    sorting out duplicates

--- caller/adder/__init__.py
+++ caller/adder/__init__.py
@@ -1,6 +1,6 @@
 ###############################################################################
 ##
-##  Copyright (C) 2014, Tavendo GmbH and/or collaborators. All rights reserved.
+##  Copyright (C) 2015, Tavendo GmbH and/or collaborators. All rights reserved.
 ##
 ##  Redistribution and use in source and binary forms, with or without
 ##  modification, are permitted provided that the following conditions are met:
