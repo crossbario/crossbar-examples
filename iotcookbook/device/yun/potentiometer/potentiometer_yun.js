a39276b8eedef8d47c2fd33952335e034860df49
Alexander Goedde <alexander.goedde@tavendo.de>
2015-05-23 16:41:51 +0200

    potentiometer component v1

--- iotcookbook/device/yun/potentiometer/potentiometer_yun.js
+++ iotcookbook/device/yun/potentiometer/potentiometer_yun.js
@@ -92,53 +92,46 @@ arduino.on('connect', function () {
         *****************************/
 
         var pot_pin = 0; // change to suit your requirements: 'I0': 0, 'I1': 1, 'I2': 2, 'I3': 3, 'I4': 4, 'I5': 5,
-        var pot_min = 9; // minimum value the sensor produces at full dark
-        var pot_max = 880; // max value the sensor produces
+        var pot_min = 13; // minimum value the potentiometer produces
+        var pot_max = 868; // max value the potentiomenter produces
         var pot_last = 0;
         var pot_smoothe = 1; // offset below which no value changes are sent
 
-        // set_mode([pot_pin, "watch"]);
+        set_mode([pot_pin, "watch"]);
 
         var get_potentiometer_value = function () {
 
             set_mode([pot_pin, "in"]);
 
-            var rawValue = analog_read([pot_pin]);
+            var normValue = normalizeValue(analog_read([pot_pin]));
             
             set_mode([pot_pin, "watch"]);
 
-            // ldr_last = normValue;
+            pot_last = normValue;
             
-            return rawValue;
+            return normValue;
         };
 
-        // var normalizeValue = function (rawValue) {
-        //     return parseInt(((rawValue - ldr_min) / (ldr_max - ldr_min)) * 100);
-        // };
+        var normalizeValue = function (rawValue) {
+            return parseInt(((rawValue - pot_min) / (pot_max - pot_min)) * 100);
+        };
 
-        // arduino.on('analogChange', function (e) {
-        //     if (ldr_pin === e.pin) {
+        arduino.on('analogChange', function (e) {
+            if (pot_pin === e.pin) {
 
-        //         normValue = normalizeValue(e.value);
+                normValue = normalizeValue(e.value);
 
-        //         if (debug) {
-        //             console.log("light level changed", e.value, normValue, ldr_last);
-        //         }
+                if (debug) {
+                    console.log("light level changed", e.value, normValue, pot_last);
+                }
 
-        //         // constant value wanted, differs from previous value by more than smoothe value
-        //         if (threshold === 0 && (normValue > ldr_last + ldr_smoothe || normValue < ldr_last - ldr_smoothe)) {
-        //             ldr_last = normValue;
-        //             session.publish("io.crossbar.examples.yun.potentiometer.on_light_level_change", [normValue]);    
-        //         // is at other side of threshold value (with smoothe area around that value)
-        //         } else if (normValue > threshold + ldr_smoothe && ldr_last < threshold - ldr_smoothe) {
-        //             ldr_last = normValue;
-        //             session.publish("io.crossbar.examples.yun.potentiometer.on_threshold_passed", [true]);
-        //         } else if (normValue < threshold - ldr_smoothe && ldr_last > threshold + ldr_smoothe) {
-        //             ldr_last = normValue;
-        //             session.publish("io.crossbar.examples.yun.potentiometer.on_threshold_passed", [false]);
-        //         }               
-        //     }
-        // });
+                // constant value wanted, differs from previous value by more than smoothe value
+                if (normValue > pot_last + pot_smoothe || normValue < pot_last - pot_smoothe) {
+                    pot_last = normValue;
+                    session.publish("io.crossbar.examples.yun.potentiometer.on_value_change", [normValue]);    
+                }              
+            }
+        });
 
         session.register("io.crossbar.examples.yun.potentiometer.get_potentiometer_value", get_potentiometer_value).then(function() {
             console.log("io.crossbar.examples.yun.potentiometer.get_potentiometer_value registered");
@@ -146,16 +139,6 @@ arduino.on('connect', function () {
         function () {
             console.log("io.crossbar.examples.yun.potentiometer.get_potentiometer_value - registration error");
         });
-
-
-        // session.register("io.crossbar.examples.yun.potentiometer.set_threshold", function(args) {
-        //     threshold = args[0];
-        // }).then(function() {
-        //     console.log("io.crossbar.examples.yun.potentiometer.set_threshold registered");
-        // }, 
-        // function () {
-        //     console.log("io.crossbar.examples.yun.potentiometer.set_threshold - registration error");
-        // });
     };
 
     connection.onclose = function (reason, details) {
