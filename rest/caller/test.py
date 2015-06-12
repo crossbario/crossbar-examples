33c0482e74ec97cc4f3fcc6de5b4f5f325a49522
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-06-12 05:43:40 +0200

    fix test driver

--- rest/caller/test.py
+++ rest/caller/test.py
@@ -1,11 +1,10 @@
 proc = 'com.example.add2'
-args = None
+args = [2, 3]
 kwargs = None
 options = None
 
 def test():
     import requests, json
-    print("foobar")
     payload = {"procedure": proc}
     if args:
         payload['args'] = list(args)
@@ -13,8 +12,7 @@ def test():
         payload['kwargs'] = dict(kwargs)
     headers = {'Content-Type': 'application/json'}
     r = requests.post("http://127.0.0.1:8080/call", data=json.dumps(payload), headers=headers)
-    return r.text
     res = r.json()
     return res['args'][0]
 
-test()
+print test()
