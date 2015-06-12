b181c7474e871f984ee70183200486304af31e8c
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-06-12 07:27:41 +0200

    sync

--- rest/caller/test.py
+++ rest/caller/test.py
@@ -13,7 +13,7 @@ def test():
     if kwargs:
         payload['kwargs'] = dict(kwargs)
     headers = {'Content-Type': 'application/json'}
-    r = requests.post("http://127.0.0.1:8080/call", data=json.dumps(payload), headers=headers)
+    r = requests.post("http://10.0.1.2:8080/call", data=json.dumps(payload), headers=headers)
     res = r.json()
     return res['args'][0]
 
