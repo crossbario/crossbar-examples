0f3bf5863fb8664317bde63a36de436f7bea2d41
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-06-16 13:11:17 +0200

    add Py test client

---
+++ rest/caller/test.py
@@ -0,0 +1,20 @@
+proc = 'com.example.add2'
+proc = 'com.example.mul2'
+
+args = [2, 3]
+kwargs = None
+options = None
+
+def test():
+    import requests, json
+    payload = {"procedure": proc}
+    if args:
+        payload['args'] = list(args)
+    if kwargs:
+        payload['kwargs'] = dict(kwargs)
+    headers = {'Content-Type': 'application/json'}
+    r = requests.post("http://10.0.1.2:8080/call", data=json.dumps(payload), headers=headers)
+    res = r.json()
+    return res['args'][0]
+
+print test()
