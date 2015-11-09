9f0858e5166abc4e4f29fe113721fbfa4ea480ed
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-11-09 20:46:26 +0100

    sync

--- work/mcweb/myresource.py
+++ work/mcweb/myresource.py
@@ -4,9 +4,14 @@ from twisted.web import server, resource
 class MyResource(resource.Resource):
     isLeaf = True
 
-    def __init__(self, what):
-        print(what)
+    def __init__(self, extra):
         resource.Resource.__init__(self)
 
     def render_GET(self, request):
-        return "<html>Hello, world!</html>"
+        count = 10
+        if 'count' in request.args:
+            try:
+                count = int(request.args['count'][-1])
+            except ValueError:
+                pass
+        return '*' * count
