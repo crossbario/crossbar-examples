850e8517fe7224208d3a915feae0fd45e46030a8
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-11-09 20:38:50 +0100

    fixes

--- work/mcweb/myresource.py
+++ work/mcweb/myresource.py
@@ -1,8 +1,12 @@
 from twisted.web import server, resource
 
 
-class MyResouce(resource.Resource):
+class MyResource(resource.Resource):
     isLeaf = True
+
+    def __init__(self, what):
+        print(what)
+        resource.Resource.__init__(self)
 
     def render_GET(self, request):
         return "<html>Hello, world!</html>"
