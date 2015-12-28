70019b5a6d7e7d158cf80835e6322d58e3b88a4b
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-12-29 00:13:56 +0100

    more example code

--- payload-encryption/client1.py
+++ payload-encryption/client1.py
@@ -31,9 +31,10 @@ from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.util import sleep
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 from autobahn.wamp.types import PublishOptions, CallOptions
+from autobahn.wamp.exception import ApplicationError
 from autobahn.wamp.keyring import KeyRing, Key
 
-from sample_keys import ORIGINATOR_PRIV, RESPONDER_PUB
+from sample_keys import PRIVKEY, ORIGINATOR_PRIV, RESPONDER_PUB
 
 
 class Component1(ApplicationSession):
@@ -45,18 +46,21 @@ class Component1(ApplicationSession):
         print("joined")
 
         # setup application payload end-to-end encryption ("WAMP-cryptobox")
+        # when a keyring was set, end-to-end encryption is performed automatically
+        if False:
+            keyring = KeyRing(PRIVKEY)
+        else:
+            # we need to have a keyring first. create an empty one.
+            keyring = KeyRing()
 
-        # we need to have a keyring first. create an empty one.
-        keyring = KeyRing()
+            # since we want to act as "caller" (and "publisher"), we are thus a "originator"
+            # and originators need the originator private key. however, we don't act as "callees"
+            # (or "subscribers"), and hence can get away with the public key for the responder only!
+            key = Key(originator_priv=ORIGINATOR_PRIV, responder_pub=RESPONDER_PUB)
 
-        # since we want to act as "caller" (and "publisher"), we are thus a "originator"
-        # and originators need the originator private key. however, we don't act as "callees"
-        # (or "subscribers"), and hence can get away with the public key for the responder only!
-        key = Key(originator_priv=ORIGINATOR_PRIV, responder_pub=RESPONDER_PUB)
-
-        # we now associate URIs starting with "com.myapp.encrypted." with the
-        # encryption keys ..
-        keyring.set_key(u'com.myapp.encrypted.', key)
+            # we now associate URIs starting with "com.myapp.encrypted." with the
+            # encryption keys ..
+            keyring.set_key(u'com.myapp.encrypted.', key)
 
         # .. and finally set the keyring on the session. from now on, all calls (and event)
         # on URIs that start with "com.myapp.encrypted." will be encrypted. Calls (and events)
@@ -65,23 +69,45 @@ class Component1(ApplicationSession):
 
         # now start the testing ..
 
-        yield self._test_rpc()
-        yield self._test_pubsub()
+        #yield self._test_rpc()
+        yield self._test_rpc_errors()
+        #yield self._test_pubsub()
 
         print("done!")
         self.leave()
 
     @inlineCallbacks
-    def _test_rpc(self):
+    def _test_rpc(self, delay=None):
         options = CallOptions(disclose_me=True)
+
         counter = 1
         while counter <= self.NUM:
-            res = yield self.call(u'com.myapp.add2', 23, counter, options=options)
-            print("called: {}".format(res))
-            res = yield self.call(u'com.myapp.encrypted.add2', 23, counter, options=options)
-            print("called: {}".format(res))
-            yield sleep(1)
+            for proc in [u'com.myapp.add2',
+                         u'com.myapp.encrypted.add2']:
+                try:
+                    res = yield self.call(proc, 23, counter, options=options)
+                    print("{} call result: {}".format(proc, res))
+                except Exception as e:
+                    print("{} call error: {}".format(proc, e))
+
+            if delay:
+                yield sleep(delay)
             counter += 1
+
+    @inlineCallbacks
+    def _test_rpc_errors(self):
+        options = CallOptions(disclose_me=True)
+
+        for proc in [(u'com.myapp.failme', False),
+                     (u'com.myapp.failme', True),
+                     (u'com.myapp.encrypted.failme', False),
+                     (u'com.myapp.encrypted.failme', True)]:
+            try:
+                res = yield self.call(proc[0], proc[1], options=options)
+                print("{} call with {} result: {}".format(proc[0], proc[1], res))
+            except ApplicationError as e:
+                print(type(e))
+                print("{} call with {} error: {}".format(proc[0], proc[1], e))
 
     @inlineCallbacks
     def _test_pubsub(self):
--- payload-encryption/client2.py
+++ payload-encryption/client2.py
@@ -31,8 +31,9 @@ from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 from autobahn.wamp.types import SubscribeOptions, RegisterOptions
 from autobahn.wamp.keyring import KeyRing, Key
+from autobahn.wamp.exception import ApplicationError
 
-from sample_keys import RESPONDER_PRIV, ORIGINATOR_PUB
+from sample_keys import PRIVKEY, RESPONDER_PRIV, ORIGINATOR_PUB
 
 
 class Component2(ApplicationSession):
@@ -42,28 +43,45 @@ class Component2(ApplicationSession):
         print("joined")
 
         # setup application payload end-to-end encryption ("WAMP-cryptobox")
+        # when a keyring was set, end-to-end encryption is performed automatically
+        if False:
+            keyring = KeyRing(PRIVKEY)
+        else:
+            # this works the same as in Component1, but the keys
+            # loaded is different.
+            keyring = KeyRing()
 
-        # this works the same as in Component1, but the keys
-        # loaded is different.
-        keyring = KeyRing()
+            # since we want to act as "callee" (and "subscriber"), we are thus a "responder"
+            # and responders need the responder private key. however, we don't act as "callers"
+            # (or "publishers"), and hence can get away with the public key for the originator only!
+            key = Key(responder_priv=RESPONDER_PRIV, originator_pub=ORIGINATOR_PUB)
+            keyring.set_key(u'com.myapp.encrypted.', key)
 
-        # since we want to act as "callee" (and "subscriber"), we are thus a "responder"
-        # and responders need the responder private key. however, we don't act as "callers"
-        # (or "publishers"), and hence can get away with the public key for the originator only!
-        key = Key(responder_priv=RESPONDER_PRIV, originator_pub=ORIGINATOR_PUB)
-        keyring.set_key(u'com.myapp.encrypted.', key)
         self.set_keyring(keyring)
 
         # now start the testing ..
 
         def add2(a, b, details=None):
             print("call received: a={}, b={}, details={}".format(a, b, details))
+
+            # when the procedure args were encrypted, the result will be always encrypted too!
             return a + b
 
         options = RegisterOptions(details_arg='details')
         reg1 = yield self.register(add2, u'com.myapp.add2', options=options)
         reg2 = yield self.register(add2, u'com.myapp.encrypted.add2', options=options)
 
+        def failme(encrypted_error, details=None):
+            # independent of whether the "failme" procedure args were encrypted or not,
+            # an error returned to the caller will be encrypted or not depending soley
+            # on the error URI
+            if encrypted_error:
+                raise ApplicationError(u"com.myapp.encrypted.error1", custom1=23, custom2=u'Hello')
+            else:
+                raise ApplicationError(u"com.myapp.error1", custom1=23, custom2=u'Hello')
+
+        reg3 = yield self.register(failme, u'com.myapp.failme', options=options)
+        reg4 = yield self.register(failme, u'com.myapp.encrypted.failme', options=options)
 
         def on_hello(msg, details=None):
             print("event received: msg='{}', details={}".format(msg, details))
