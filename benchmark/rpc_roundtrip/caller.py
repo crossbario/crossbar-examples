e9aea4a13743e3c6a5606ce4b2aad3b8377ca524
Tobias Oberstein <tobias.oberstein@tavendo.de>
2016-02-15 20:40:39 +0100

    sync

--- benchmark/rpc_roundtrip/caller.py
+++ benchmark/rpc_roundtrip/caller.py
@@ -1,6 +1,9 @@
 from time import time
+import argparse
+import six
 from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession
+from autobahn.twisted.wamp import ApplicationRunner
 from autobahn.twisted.util import sleep
 
 
@@ -23,3 +26,21 @@ class AppSession(ApplicationSession):
                 avg_rtt = 1000. * float(ts_end - ts_start) / float(batch_size)
                 print("[{}] - average round-trip time (ms): {}".format(logname, avg_rtt))
                 ts_start = ts_end
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--router", type=six.text_type, default=u"ws://10.1.1.11:9000", help='WAMP router URL.')
+    parser.add_argument("--realm", type=six.text_type, default=u"realm1", help='WAMP router realm.')
+
+    args = parser.parse_args()
+
+    extra = {
+        u'prefix': u'com.example.websocket-json',
+        u'logname': u'WAMP-TCP-WebSocket-JSON'
+    }
+
+    runner = ApplicationRunner(url=args.router, realm=args.realm, extra=extra)
+    runner.run(AppSession)
+
