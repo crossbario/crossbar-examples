f6239da687a60984cba501e42c6e376ed67cb690
Tobias Oberstein <tobias.oberstein@tavendo.de>
2016-02-18 19:05:40 +0100

    some fixes

--- authentication/cryptosign/static/client_raw_key.py
+++ authentication/cryptosign/static/client_raw_key.py
@@ -26,106 +26,138 @@
 ##
 ###############################################################################
 
+import txaio
+txaio.use_twisted()
+
 from twisted.internet import reactor
+from twisted.internet.error import ReactorNotRunning
 from autobahn.twisted.wamp import ApplicationSession
 from autobahn.wamp import cryptosign
 
 
 class ClientSession(ApplicationSession):
 
-   def __init__(self, config=None):
-      print("__init__(config={})".format(config))
-      ApplicationSession.__init__(self, config)
+    def __init__(self, config=None):
+        self.log.info("initializing component: {config}", config=config)
+        ApplicationSession.__init__(self, config)
 
-      # load the client private key (raw format)
-      self._key = cryptosign.Key.from_raw(config.extra[u'key'])
-      print("Client public key loaded: {}".format(self._key.public_key()))
+        # load the client private key (raw format)
+        try:
+            self._key = cryptosign.SigningKey.from_raw_key(config.extra[u'key'])
+        except Exception as e:
+            self.log.error("could not load client private key: {log_failure}", log_failure=e)
+            self.leave()
+        else:
+            self.log.info("client public key loaded: {}".format(self._key.public_key()))
 
-   def onConnect(self):
-      print("onConnect()")
+    def onConnect(self):
+        self.log.info("connected to router")
 
-      # authentication extra information for wamp-cryptosign
-      #
-      extra = {
-         # forward the client pubkey: this allows us to omit authid as
-         # the router can identify us with the pubkey already
-         u'pubkey': self._key.public_key(),
+        # authentication extra information for wamp-cryptosign
+        #
+        extra = {
+            # forward the client pubkey: this allows us to omit authid as
+            # the router can identify us with the pubkey already
+            u'pubkey': self._key.public_key(),
 
-         # not yet implemented. a public key the router should provide
-         # a trustchain for it's public key. the trustroot can eg be
-         # hard-coded in the client, or come from a command line option.
-         u'trustroot': None,
+            # not yet implemented. a public key the router should provide
+            # a trustchain for it's public key. the trustroot can eg be
+            # hard-coded in the client, or come from a command line option.
+            u'trustroot': None,
 
-         # not yet implemented. for authenticating the router, this
-         # challenge will need to be signed by the router and send back
-         # in AUTHENTICATE for client to verify. A string with a hex
-         # encoded 32 bytes random value.
-         u'challenge': None
-      }
+            # not yet implemented. for authenticating the router, this
+            # challenge will need to be signed by the router and send back
+            # in AUTHENTICATE for client to verify. A string with a hex
+            # encoded 32 bytes random value.
+            u'challenge': None
+        }
 
-      # now request to join ..
-      self.join(self.config.realm,
-                authmethods=[u'cryptosign'],
-                authid=self.config.extra[u'authid'],
-                authextra=extra)
+        # now request to join ..
+        self.join(self.config.realm,
+                  authmethods=[u'cryptosign'],
+                  authid=self.config.extra[u'authid'],
+                  authextra=extra)
 
-   def onChallenge(self, challenge):
-      print("onChallenge(challenge={})".format(challenge))
-      # alright, we've got a challenge from the router.
+    def onChallenge(self, challenge):
+        self.log.info("authentication challenge received: {challenge}", challenge=challenge)
+        # alright, we've got a challenge from the router.
 
-      # not yet implemented. check the trustchain the router provided against
-      # our trustroot, and check the signature provided by the
-      # router for our previous challenge. if both are ok, everything
-      # is fine - the router is authentic wrt our trustroot.
+        # not yet implemented. check the trustchain the router provided against
+        # our trustroot, and check the signature provided by the
+        # router for our previous challenge. if both are ok, everything
+        # is fine - the router is authentic wrt our trustroot.
 
-      # sign the challenge with our private key.
-      signed_challenge = self._key.sign_challenge(challenge)
+        # sign the challenge with our private key.
+        signed_challenge = self._key.sign_challenge(self, challenge)
 
-      # send back the signed challenge for verification
-      return signed_challenge
+        # send back the signed challenge for verification
+        return signed_challenge
 
-   def onJoin(self, details):
-      print("onJoin(details={})".format(details))
-      print("""
-            Hooray! We've been successfully authenticated
-            with WAMP-cryptosign using Ed25519!
-            """)
-      self.leave()
+    def onJoin(self, details):
+        self.log.info("session joined: {details}", details=details)
+        self.log.info("*** Hooray! We've been successfully authenticated with WAMP-cryptosign using Ed25519! ***")
+        self.leave()
 
-   def onLeave(self, details):
-      print("onLeave(details={})".format(details))
-      self.disconnect()
+    def onLeave(self, details):
+        self.log.info("session closed: {details}", details=details)
+        self.disconnect()
 
-   def onDisconnect(self):
-      print("onDisconnect()")
-      reactor.stop()
+    def onDisconnect(self):
+        self.log.info("connection to router closed")
+        try:
+            reactor.stop()
+        except ReactorNotRunning:
+            pass
 
 
 if __name__ == '__main__':
 
-   import six
-   import sys
-   import argparse
-   from autobahn.twisted.wamp import ApplicationRunner
+    import six
+    import sys
+    import argparse
+    from autobahn.twisted.wamp import ApplicationRunner
 
-   # parse command line arguments
-   parser = argparse.ArgumentParser()
-   parser.add_argument('--authid', dest='authid', type=six.text_type, default=None, help='The authid to connect under. If not provided, let the router auto-choose the authid.')
-   parser.add_argument('--realm', dest='realm', type=six.text_type, default=None, help='The realm to join. If not provided, let the router auto-choose the realm.')
-   parser.add_argument('--key', dest='key', type=six.text_type, required=True, help='The private client key to use for authentication. A 32 bytes file containing the raw Ed25519 private key.')
-   parser.add_argument('--routerkey', dest='routerkey', type=six.text_type, default=None, help='The public router key to verify the remote router against. A 32 bytes file containing the raw Ed25519 public key.')
-   parser.add_argument('--url', dest='url', type=six.text_type, default=u'ws://localhost:8080/ws', help='The router URL (default: ws://localhost:8080/ws).')
-   parser.add_argument('--agent', dest='agent', type=six.text_type, default=None, help='Path to Unix domain socket of SSH agent to use.')
-   parser.add_argument('--trace', dest='trace', action='store_true', default=False, help='Trace traffic: log WAMP messages sent and received')
-   options = parser.parse_args()
+    # parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', dest='debug', action='store_true', default=False, help='Enable logging at level "debug".')
+    parser.add_argument('--authid', dest='authid', type=six.text_type, default=None, help='The authid to connect under. If not provided, let the router auto-choose the authid.')
+    parser.add_argument('--realm', dest='realm', type=six.text_type, default=None, help='The realm to join. If not provided, let the router auto-choose the realm.')
+    parser.add_argument('--key', dest='key', type=six.text_type, required=True, help='The private client key to use for authentication. A 32 bytes file containing the raw Ed25519 private key.')
+    parser.add_argument('--routerkey', dest='routerkey', type=six.text_type, default=None, help='The public router key to verify the remote router against. A 32 bytes file containing the raw Ed25519 public key.')
+    parser.add_argument('--url', dest='url', type=six.text_type, default=u'wss://localhost:8080/ws', help='The router URL (default: ws://localhost:8080/ws).')
+    parser.add_argument('--agent', dest='agent', type=six.text_type, default=None, help='Path to Unix domain socket of SSH agent to use.')
+    options = parser.parse_args()
 
-   # forward requested authid and key filename to ClientSession
-   extra = {
-      u'authid': options.authid,
-      u'key': options.key
-   }
-   print("Connecting to {}: realm={}, authid={}".format(options.url, options.realm, options.authid))
+    if options.debug:
+        txaio.start_logging(level='debug')
+    else:
+        txaio.start_logging(level='info')
 
-   # connect to router and run ClientSession
-   runner = ApplicationRunner(url=options.url, realm=options.realm, extra=extra, debug_wamp=options.trace)
-   runner.run(ClientSession)
+
+    # create client SSL context from router CA and intermediate certificate
+    from twisted.internet.ssl import Certificate
+    from twisted.internet.ssl import optionsForClientTLS
+    from twisted.internet._sslverify import OpenSSLCertificateAuthorities
+    from OpenSSL import crypto
+    import six
+
+    ca_certs = []
+    for cert_fname in ['.crossbar/ca.cert.pem', '.crossbar/intermediate.cert.pem']:
+        cert = crypto.load_certificate(
+            crypto.FILETYPE_PEM,
+            six.u(open(cert_fname, 'r').read())
+        )
+        ca_certs.append(cert)
+    ca_certs = OpenSSLCertificateAuthorities(ca_certs)
+    ctx = optionsForClientTLS(u'localhost', trustRoot=ca_certs)
+
+    # forward requested authid and key filename to ClientSession
+    extra = {
+        u'authid': options.authid,
+        u'key': options.key
+    }
+    print("Connecting to {}: realm={}, authid={}".format(options.url, options.realm, options.authid))
+
+    # connect to router and run ClientSession
+    runner = ApplicationRunner(url=options.url, ssl=ctx, realm=options.realm, extra=extra)
+    runner.run(ClientSession)
