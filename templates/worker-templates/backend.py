5219c7244595f466be6e85075a2f3574427db36f
Tobias Oberstein <tobias.oberstein@tavendo.de>
2016-03-23 00:41:11 +0100

    working with glitches

--- templates/worker-templates/backend.py
+++ templates/worker-templates/backend.py
@@ -4,4 +4,4 @@ from autobahn.twisted.wamp import ApplicationSession
 class Backend(ApplicationSession):
 
    def onJoin(self, details):
-      self.log.info("Backend connected: {details}", details=details)
+      self.log.info("Backend connected: realm '{realm}', authid '{authid}'", realm=details.realm, authid=details.authid)
