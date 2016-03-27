b4da8838a65858e694efdf5338452ef7e4fb133b
Tobias Oberstein <tobias.oberstein@tavendo.de>
2016-03-27 23:58:49 +0200

    polish

--- docker/disclose/client/client.py
+++ docker/disclose/client/client.py
@@ -1,46 +1,16 @@
-###############################################################################
-#
-# Copyright (C) 2014, Tavendo GmbH and/or collaborators. All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-###############################################################################
-
-from twisted.internet.defer import inlineCallbacks
-from twisted.logger import Logger
-
-from autobahn.twisted.util import sleep
-from autobahn.twisted.wamp import ApplicationSession
-from autobahn.wamp.exception import ApplicationError
-from autobahn.wamp.types import SubscribeOptions, RegisterOptions
-
 import argparse
 import six
 import txaio
 
 from twisted.internet import reactor
 from twisted.internet.error import ReactorNotRunning
+from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
+
+from autobahn.twisted.util import sleep
+from autobahn.twisted.wamp import ApplicationSession
+from autobahn.wamp.exception import ApplicationError
+from autobahn.wamp.types import SubscribeOptions, RegisterOptions
 
 
 class ClientSession(ApplicationSession):
@@ -71,7 +41,6 @@ class ClientSession(ApplicationSession):
         self.log.info("Client session joined {details}", details=details)
 
         # SUBSCRIBE to a topic and receive events
-        #
         def onhello(msg, details=None):
             self.log.info("event for 'onhello' received: {msg} {details}", msg=msg, details=details)
 
@@ -79,7 +48,6 @@ class ClientSession(ApplicationSession):
         self.log.info("subscribed to topic 'onhello'")
 
         # REGISTER a procedure for remote calling
-        #
         def add2(x, y, details=None):
             self.log.info("add2() called with {x} and {y} - {details}", x=x, y=y, details=details)
             return x + y
@@ -88,19 +56,16 @@ class ClientSession(ApplicationSession):
         self.log.info("procedure add2() registered")
 
         # PUBLISH and CALL every second .. forever
-        #
         counter = 0
         while True:
 
             # PUBLISH an event
-            #
             yield self.publish(u'com.example.oncounter', counter)
             self.log.info("published to 'oncounter' with counter {counter}",
                           counter=counter)
             counter += 1
 
             # CALL a remote procedure
-            #
             try:
                 res = yield self.call(u'com.example.mul2', counter, 3)
                 self.log.info("mul2() called with result: {result}",
