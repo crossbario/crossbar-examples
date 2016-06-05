7b2053a32448a6ba5d8c84c8b7829d08b3502bff
Tobias Oberstein <tobias.oberstein@tavendo.de>
2016-06-05 23:00:16 +0200

    sync

--- iotcookbook/device/pi/neopixel/client.py
+++ iotcookbook/device/pi/neopixel/client.py
@@ -1,5 +1,6 @@
 import time
 import random
+import Adafruit_ADS1x15
 from neopixel import *
 
 LED_COUNT      = 8       # Number of LED pixels.
@@ -9,31 +10,8 @@ LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 100     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 
-def wheel(pos):
-	"""Generate rainbow colors across 0-255 positions."""
-	if pos < 85:
-		return Color(pos * 3, 255 - pos * 3, 0)
-	elif pos < 170:
-		pos -= 85
-		return Color(255 - pos * 3, 0, pos * 3)
-	else:
-		pos -= 170
-		return Color(0, pos * 3, 255 - pos * 3)
 
-def rainbowCycle(strip, wait_ms=5, iterations=5):
-	for j in range(256*iterations):
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel(((i * 256 / strip.numPixels()) + j) & 255))
-		strip.show()
-		time.sleep(wait_ms/1000.0)
-
-def randomCycle(strip):
-	for i in range(strip.numPixels()):
-		strip.setPixelColor(i, Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-	strip.show()
-	time.sleep(50./1000.)
-
-def levelMeter(strip, value):
+def show_level(strip, value):
 	i = int(round(float(strip.numPixels()) * value))
 	for k in range(strip.numPixels()):
 		if k < i:
@@ -41,14 +19,14 @@ def levelMeter(strip, value):
 		else:
 			strip.setPixelColor(k, Color(0, 0, 0))
 	strip.show()
-	time.sleep(50./1000.)
 
 if __name__ == '__main__':
+	adc = Adafruit_ADS1x15.ADS1015()
 	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 	strip.begin()
 
 	while True:
-		#rainbowCycle(strip)
-		#randomCycle(strip)
-		levelMeter(strip, .5)
-
+		value = adc.read_adc(0, gain=4)
+		print(value)
+		show_level(strip, .5)
+		time.sleep(.2)
