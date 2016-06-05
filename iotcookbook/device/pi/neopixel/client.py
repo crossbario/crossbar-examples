272fb08bdaf976ac9803c0ead58b7f183184e13e
Tobias Oberstein <tobias.oberstein@tavendo.de>
2016-06-05 22:28:04 +0000

    sync

--- iotcookbook/device/pi/neopixel/client.py
+++ iotcookbook/device/pi/neopixel/client.py
@@ -7,7 +7,7 @@ LED_COUNT      = 8       # Number of LED pixels.
 LED_PIN        = 12      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 100     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 80     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 
 
@@ -15,7 +15,7 @@ def show_level(strip, value):
 	i = int(round(float(strip.numPixels()) * value))
 	for k in range(strip.numPixels()):
 		if k < i:
-			strip.setPixelColor(k, Color(255, 222, 0))
+			strip.setPixelColor(k, Color(255, 255, 255))
 		else:
 			strip.setPixelColor(k, Color(0, 0, 0))
 	strip.show()
@@ -25,8 +25,15 @@ if __name__ == '__main__':
 	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 	strip.begin()
 
-	while True:
-		value = adc.read_adc(0, gain=4)
-		print(value)
-		show_level(strip, .5)
-		time.sleep(.2)
+	while False:
+		c = 800.
+		value = 0
+		for i in range(2):
+			value += (c - float(adc.read_adc(0, gain=8))) / c
+		show_level(strip, value / 2.)
+		time.sleep(20./1000.)
+
+	for i in range(strip.numPixels()):
+		strip.setPixelColor(i, Color(0, 0, 0))
+	strip.show()
+
