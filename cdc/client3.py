22256e95d02a8de84e2a7ac72b155440694e8f8d
Tobias Oberstein <tobias.oberstein@tavendo.de>
2016-08-11 19:01:30 +0200

    sync

--- cdc/client3.py
+++ cdc/client3.py
@@ -9,7 +9,12 @@ from autobahn.wamp import cryptosign
 #from autobahn.asyncio.wamp import ApplicationSession, ApplicationRunner
 
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
+from autobahn.wamp.types import SubscribeOptions
 from twisted.internet.defer import inlineCallbacks
+
+from twisted.internet import reactor
+from twisted.internet.error import ReactorNotRunning
+
 
 class MyComponent(ApplicationSession):
 
@@ -60,63 +65,19 @@ class MyComponent(ApplicationSession):
     @inlineCallbacks
     def onJoin(self, details):
         self.log.info("session joined: {details}", details=details)
-        self.log.info("*** Hooray! We've been successfully authenticated with WAMP-cryptosign using Ed25519! ***")
-
-        try:
-            now = yield self.call(u'com.crossbario.cdc.api.get_now')
-        except:
-            self.log.failure()
-        else:
-            self.log.info("Current time at CDC: {now}", now=now)
 
         try:
             node_id = u'node1'
             container_id = u'ctr01'
             node_info = yield self.call(u'com.crossbario.cdc.api.get_node_info', node_id)
+            #self.log.info(node_info)
             yield self.call(u'com.crossbario.cdc.api.start_container_worker', node_id, container_id)
         except:
             self.log.failure()
         else:
             self.log.info('Container "{container_id}" started on node "{node_id}"', node_id=node_id, container_id=container_id)
-
-        try:
-            nodes = yield self.call(u'com.crossbario.cdc.api.get_nodes')
-            self.log.info("Nodes in management realm: {nodes}", nodes=nodes)
-            for node in nodes:
-                node_id = node[u'node_id']
-                if node[u'status'] == u'running':
-                    node_info = yield self.call(u'com.crossbario.cdc.api.get_node_info', node_id)
-                    self.log.info('Node info for "{node_id}": {node_info}', node_id=node_id, node_info=node_info)
-
-                    controller_info = yield self.call(u'com.crossbario.cdc.api.get_controller_info', node_id)
-                    self.log.info('Controller info for "{node_id}": {controller_info}', node_id=node_id, controller_info=controller_info)
-
-                    controller_stats = yield self.call(u'com.crossbario.cdc.api.get_controller_stats', node_id)
-                    self.log.info('Controller stats for "{node_id}": {controller_stats}', node_id=node_id, controller_stats=controller_stats)
-
-                    workers = yield self.call(u'com.crossbario.cdc.api.get_node_workers', node_id)
-                    for worker in workers:
-                        worker_id = worker[u'id']
-                        self.log.info('Worker "{worker_id}" running on node "{node_id}": {worker}', node_id=node_id, worker_id=worker_id, worker=worker)
-                        if worker[u'status'] == u'started':
-
-                            worker_cpu_affinity = yield self.call(u'com.crossbario.cdc.api.get_worker_cpu_affinity', node_id, worker_id)
-                            self.log.info('Worker "{worker_id}" CPU affinity is {cpu_affinity}', worker_id=worker_id, cpu_affinity=worker_cpu_affinity)
-
-                            if worker[u'type'] == u'router':
-                                router_realms = yield self.call(u'com.crossbario.cdc.api.get_router_realms', node_id, worker_id)
-                                self.log.info('Realms on node "{node_id}" / router "{worker_id}": {router_realms}', node_id=node_id, worker_id=worker_id, router_realms=router_realms)
-
-                            worker_log = yield self.call(u'com.crossbario.cdc.api.get_worker_log', node_id, worker_id, 20)
-                            for line in worker_log:
-                                self.log.info("{line}", line=line)
-                            #self.log.info('Worker log: {worker_log}', worker_log=worker_log)
-                else:
-                    self.log.info('Node "{node_id}" is not running', node_id=node_id)
-        except:
-            self.log.failure()
-
-        self.leave()
+        finally:
+            self.leave()
 
     def onLeave(self, details):
         self.log.info("session closed: {details}", details=details)
@@ -124,6 +85,10 @@ class MyComponent(ApplicationSession):
 
     def onDisconnect(self):
         self.log.info("connection to router closed")
+        try:
+            reactor.stop()
+        except ReactorNotRunning:
+            pass
 
 
 if __name__ == '__main__':
