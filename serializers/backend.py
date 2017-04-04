da917115d22b7361c90e6570c8f78fc3e07e604e
Tobias Oberstein <tobias.oberstein@crossbario.com>
2017-04-04 22:54:45 +0200

    make it break

--- serializers/backend.py
+++ serializers/backend.py
@@ -1,14 +1,34 @@
+import os
+import six
 from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession
+from autobahn.wamp.exception import ApplicationError
+
 
 class Backend(ApplicationSession):
 
     @inlineCallbacks
     def onJoin(self, details):
 
+        self.log.info("BACKEND joined: {details}", details=details)
+
         def add2(x, y):
-            self.log.info("add2() called with {x} and {y}", x=x, y=y)
+            self.log.info("add2({x}, {y}) called", x=x, y=y)
             return x + y
 
-        reg = yield self.register(add2, u'com.example.add2')
-        self.log.info("procedure add2() registered")
+        yield self.register(add2, u'com.example.add2')
+
+        def rand(n=8):
+            self.log.info("rand(n={n}) called", n=n)
+            return os.urandom(n)
+
+        yield self.register(rand, u'com.example.rand')
+
+        def blen(binary):
+            if type(binary) != six.binary_type:
+                raise ApplicationError(u'com.example.error.invalid_type', u'expected binary, got {}'.format(type(binary)))
+            return len(binary)
+
+        yield self.register(blen, u'com.example.blen')
+
+        self.log.info("BACKEND READY!")
