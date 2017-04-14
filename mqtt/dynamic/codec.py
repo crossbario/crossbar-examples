cdc529383820b2d4a5a21953687e32d074c0112e
Tobias Oberstein <tobias.oberstein@crossbario.com>
2017-04-14 17:37:16 +0200

    actual encode/decode

--- mqtt/dynamic/codec.py
+++ mqtt/dynamic/codec.py
@@ -1,6 +1,13 @@
 from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession
 
+import struct
+import binascii
+
+# binary payload format we use in this example:
+# unsigned short + signed int + 8 bytes (all big endian)
+FORMAT = '>Hl8s'
+
 
 class MyCodec(ApplicationSession):
 
@@ -8,14 +15,17 @@ class MyCodec(ApplicationSession):
     def onJoin(self, details):
 
         def decode(mapped_topic, topic, payload):
+            pid, seq, ran = struct.unpack(FORMAT, payload)
             options = {
-                'args': None,
-                'kwargs': None
+                'args': [pid, seq, ran]
             }
+            self.log.info('decode {mapped_topic}: {from_mqtt} -> {to_wamp}', mapped_topic=mapped_topic, from_mqtt=payload, to_wamp=options)
             return options
 
         def encode(mapped_topic, topic, args, kwargs):
-            payload = b''
+            pid, seq, ran = args
+            payload = struct.pack(FORMAT, pid, seq, ran)
+            self.log.info('encode {mapped_topic}: {from_wamp} -> {to_mqtt}', mapped_topic=mapped_topic, from_wamp={u'args': args}, to_mqtt=payload)
             return payload
 
         prefix = u'com.example.mqtt'
