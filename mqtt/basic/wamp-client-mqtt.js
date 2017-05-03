9b40df40cc12cd3cf74bd1aa58932440a05ca084
Tobias Oberstein <tobias.oberstein@crossbario.com>
2017-05-03 17:56:38 +0200

    polish

--- mqtt/basic/wamp-client-mqtt.js
+++ mqtt/basic/wamp-client-mqtt.js
@@ -1,3 +1,6 @@
 // this requires similar hooks like ApplicationSession.set_payload_codec
 // in AutobahnJS first. when the hooks are there, this example can be created
-// to complete the spectrum of test clients
+// to complete the spectrum of test clients:
+// https://github.com/crossbario/autobahn-js/issues/297
+
+console.log("example client not implemented yet! FIXME (depends on https://github.com/crossbario/autobahn-js/issues/297)")
--- mqtt/basic/wamp-client.js
+++ mqtt/basic/wamp-client.js
@@ -1,1 +1,3 @@
 // FIXME: this should be just a regular AutobahnJS based client
+
+console.log("example client not implemented yet! FIXME")
