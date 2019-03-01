1ca06552ee79b622e6b087a081e048856edaf29c
Tobias Oberstein <tobias.oberstein@crossbario.com>
2019-03-01 20:51:47 +0100

    sync

--- benchmark/websocket/k6-ws-echo.js
+++ benchmark/websocket/k6-ws-echo.js
@@ -5,33 +5,29 @@ import { check } from "k6";
 // https://docs.k6.io/docs/socketsend
 //const msg = Array(262144).join('x');
 const msg = Array(64).join('x');
+const msg_count = 268435456 / msg.length;
 
 export default function() {
-  var url = "ws://127.0.0.1:8080";
-  var params = { "tags": { "my_tag": "hello" } };
-  var count = 100;
+    var url = "ws://127.0.0.1:8080";
+    var count = msg_count;
 
-  var res = ws.connect(url, params, function(socket) {
-    socket.on('open', function() {
-      //console.log('connected');
-      socket.send(msg);
+    var res = ws.connect(url, {}, function(socket) {
+        socket.on('open', function() {
+        socket.send(msg);
     });
 
     socket.on('message', function(data) {
-      //console.log("Message received: ", data);
-      count -= 1;
-      if (!count) {
-	      socket.close();
-      } else {
-	      socket.send(msg);
-      }
+        count -= 1;
+        if (!count) {
+          socket.close();
+        } else {
+          socket.send(msg);
+        }
     });
 
     socket.on('close', function() {
-      //console.log('disconnected');
     });
   });
 
   check(res, { "status is 101": (r) => r && r.status === 101 });
 }
-
