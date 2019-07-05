85942f33878af480bae7f9d6138e0463289e31ad
Tobias Oberstein <tobias.oberstein@crossbario.com>
2019-07-05 10:23:59 +0200

    sync

---
+++ containers/failures/container/restart1/myapp.py
@@ -0,0 +1,52 @@
+import os
+from pprint import pformat
+
+import txaio
+txaio.use_twisted()
+from txaio import make_logger
+
+from twisted.internet.defer import inlineCallbacks
+
+from autobahn.twisted.util import sleep
+from autobahn.wamp.types import CallDetails, RegisterOptions
+from autobahn.twisted.wamp import ApplicationSession
+from autobahn.wamp.exception import ApplicationError
+from autobahn import wamp
+
+
+class MySession(ApplicationSession):
+
+    log = make_logger()
+
+    def __init__(self, config):
+        self.log.info('MySession.__init__(config={config})', config=pformat(config))
+        ApplicationSession.__init__(self, config)
+
+    @inlineCallbacks
+    def onJoin(self, details):
+        self.log.info('MySession.onJoin(details={details})', details=details)
+
+        yield self.register(self, options=RegisterOptions(invoke='roundrobin'))
+
+        n = 2
+        while True:
+            data = os.urandom(n)
+            res = yield self.call('com.example.echo', data)
+            assert type(res) == bytes, 'result must be bytes, but was {}'.format(type(res))
+            assert len(res) == 2 * len(data)
+            self.log.info('call succeeded with result data length {}'.format(n))
+            n = n * 2
+            yield sleep(1)
+
+    @wamp.register('com.example.echo')
+    def echo(self, data, details=None):
+        assert details is None or isinstance(details, CallDetails)
+
+        if type(data) != bytes:
+            raise ApplicationError(ApplicationError.INVALID_PAYLOAD, '"data" must be bytes, but was {}'.format(type(data)))
+
+        res = data + data
+        self.log.info('{klass}.echo(): echo return {reslen} bytes',
+                      klass=self.__class__.__name__, reslen=len(res))
+
+        return res
