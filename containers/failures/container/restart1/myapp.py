619d6a1005fadc06909fdcce22c438d9babf4761
Tobias Oberstein <tobias.oberstein@crossbario.com>
2019-07-05 18:52:40 +0200

    sync

--- containers/failures/container/restart1/myapp.py
+++ containers/failures/container/restart1/myapp.py
@@ -19,7 +19,7 @@ class MySession(ApplicationSession):
     log = make_logger()
 
     def __init__(self, config):
-        self.log.info('MySession.__init__(config={config})', config=pformat(config))
+        self.log.info('MySession.__init__(config={config})', config=str(config))
         ApplicationSession.__init__(self, config)
 
     @inlineCallbacks
@@ -29,23 +29,33 @@ class MySession(ApplicationSession):
         yield self.register(self, options=RegisterOptions(invoke='roundrobin'))
 
         n = 2
-        while True:
-            data = os.urandom(n)
-            res = yield self.call('com.example.echo', data)
-            assert type(res) == bytes, 'result must be bytes, but was {}'.format(type(res))
-            assert len(res) == 2 * len(data)
-            self.log.info('call succeeded with result data length {}'.format(n))
-            n = n * 2
-            yield sleep(1)
+        running = True
+        while running:
+            data = os.urandom(n + 1)
+            try:
+                res = yield self.call('com.example.echo', data)
+            except:
+                self.log.failure()
+                running = False
+            else:
+                self.log.info('call succeeded with result data length {}'.format(n))
+                n = n * 2
+                yield sleep(1)
+
+        yield self.leave()
 
     @wamp.register('com.example.echo')
-    def echo(self, data, details=None):
+    def echo(self, data, shorten_by=None, details=None):
         assert details is None or isinstance(details, CallDetails)
 
         if type(data) != bytes:
             raise ApplicationError(ApplicationError.INVALID_PAYLOAD, '"data" must be bytes, but was {}'.format(type(data)))
 
-        res = data + data
+        res = (data + data)
+
+        if shorten_by:
+            res = res[:-shorten_by]
+
         self.log.info('{klass}.echo(): echo return {reslen} bytes',
                       klass=self.__class__.__name__, reslen=len(res))
 
