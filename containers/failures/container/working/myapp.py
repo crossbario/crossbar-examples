9ac17ccd81d0601620ee69a8331b766bea13785c
Tobias Oberstein <tobias.oberstein@crossbario.com>
2020-05-02 21:26:38 +0200

    sync

--- containers/failures/container/working/myapp.py
+++ containers/failures/container/working/myapp.py
@@ -1,14 +1,20 @@
-from twisted.logger import Logger
+import txaio
+txaio.use_twisted()
+
+from txaio import make_logger
+from twisted.internet import reactor
 from autobahn.twisted.wamp import ApplicationSession
+
 
 class MySession(ApplicationSession):
 
-    log = Logger()
+    log = make_logger()
 
     def __init__(self, config):
-        self.log.info("MySession.__init__()")
+        self.log.info('MySession.__init__(config={config})', config=config)
         ApplicationSession.__init__(self, config)
 
     def onJoin(self, details):
-        self.log.info("MySession.onJoin()")
-        self.leave()
+        self.log.info('MySession.onJoin(details={details})', details=details)
+        self.log.info('sleeping for 3s before leaving ..')
+        reactor.callLater(3, self.leave)
--- containers/failures/router/working/myapp.py
+++ containers/failures/router/working/myapp.py
@@ -1,14 +1,37 @@
-from twisted.logger import Logger
+import txaio
+txaio.use_twisted()
+
+from txaio import make_logger
+from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession
+
 
 class MySession(ApplicationSession):
 
-    log = Logger()
+    log = make_logger()
 
     def __init__(self, config):
-        self.log.info("MySession.__init__()")
+        self.log.info('MySession.__init__(config={config})', config=config)
         ApplicationSession.__init__(self, config)
 
     def onJoin(self, details):
-        self.log.info("MySession.onJoin()")
-        self.leave()
+        self.log.info('MySession.onJoin(details={details})', details=details)
+        self.log.info('sleeping for 3s before leaving ..')
+
+        @inlineCallbacks
+        def shutdown():
+            self.log.info('shutting down ..')
+            try:
+                shutting_down = yield self.config.controller.call('crossbar.shutdown')
+            except:
+                self.log.failure()
+            else:
+                self.log.info('shutdown in progress: {shutting_down}', shutting_down=shutting_down)
+            self.leave()
+
+        reactor.callLater(3, shutdown)
+
+    def onLeave(self, details):
+        self.log.info('MySession.onLeave(details={details})', details=details)
+        ApplicationSession.onLeave(self, details)
