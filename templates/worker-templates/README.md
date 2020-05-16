e7d0082fe1f777e40c51210a6bcfb1a449857172
Tobias Oberstein <tobias.oberstein@crossbario.com>
2020-05-16 08:06:25 +0200

    remove old/dead code

--- templates/worker-templates/README.md
+++
@@ -1,3 +0,0 @@
-# Worker templates
-
-This shows how to start container workers automatically whenever realms are created dynamically (from realm templates).
--- templates/worker-templates/backend.py
+++
@@ -1,7 +0,0 @@
-from autobahn.twisted.wamp import ApplicationSession
-
-
-class Backend(ApplicationSession):
-
-   def onJoin(self, details):
-      self.log.info("Backend connected: realm '{realm}', authid '{authid}'", realm=details.realm, authid=details.authid)
