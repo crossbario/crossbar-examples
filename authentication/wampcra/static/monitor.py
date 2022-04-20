56aa2bd36e8687d9d1716bf5802dbd9e913a8871
Tobias Oberstein <tobias.oberstein@crossbario.com>
2022-04-20 18:40:55 +0200

    sync

--- authentication/wampcra/static/monitor.py
+++ authentication/wampcra/static/monitor.py
@@ -33,15 +33,47 @@ class MyMonitor(ApplicationSession):
             except:
                 self.log.failure()
 
+        @inlineCallbacks
         def on_session_join(session_info):
             self.log.info('{func}: new session joined, session_info=\n{session_info}',
                           func=hltype(on_session_join), session_info=pformat(session_info))
 
+            # verify the getter API responds with the same data that we got sent in the event already
+            session_id = session_info.get('session', None)
+            if session_id:
+                try:
+                    _session_info = yield self.call('wamp.session.get', session_id)
+
+                    # FIXME
+                    for attr in ['realm', 'serializer', 'resumable', 'resume_token', 'resumed']:
+                        if attr in _session_info:
+                            del _session_info[attr]
+
+                    if session_info == _session_info:
+                        self.log.info('{func}: ok, event data identical to getter API!', func=hltype(on_session_join))
+                    else:
+                        self.log.warn('{func}: event data mismatch to getter API!\nEvent:\n{session_info_event}\nCall:\n{session_info_call}',
+                                      session_info_event=pformat(session_info),
+                                      session_info_call=pformat(_session_info),
+                                      func=hltype(on_session_join))
+                except:
+                    self.log.failure()
+
         yield self.subscribe(on_session_join, 'wamp.session.on_join')
 
+        @inlineCallbacks
         def on_session_leave(session_id):
             self.log.info('{func}: existing session left, session_id={session_id}',
                           func=hltype(on_session_join), session_id=hlid(session_id))
+
+            # ApplicationError(error=<wamp.error.no_such_session)
+            if session_id:
+                try:
+                    _session_info = yield self.call('wamp.session.get', session_id)
+                    if session_info == _session_info:
+                        self.log.info('{func}: ok, event data identical to getter API', func=hltype(on_session_join))
+                except:
+                    self.log.failure()
 
         yield self.subscribe(on_session_leave, 'wamp.session.on_leave')
 
