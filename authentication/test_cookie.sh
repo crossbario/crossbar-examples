e913300b3780c255a6902b3d878d25e274cdb519
Tobias Oberstein <tobias.oberstein@crossbario.com>
2022-04-23 13:05:20 +0200

    sync

--- authentication/test_cookie.sh
+++ authentication/test_cookie.sh
@@ -2,18 +2,52 @@
 
 ########################################################################################
 ##
-## WAMP-CRA+Ticket static
+## WAMP-CRA+Ticket static + file-store
 ##
+rm -f ./cookie/.crossbar/cookies.dat
 MYSECRET="secret123" crossbar start --cbdir=./cookie/.crossbar &
 sleep 10
 
 MYSECRET="secret123"     python ./cookie/client.py client1
-wamp_cra_cookie_good=$?
+wamp_cra_cookie_static_file_good=$?
 
 MYSECRET="wrongpassword" python ./cookie/client.py client1
-wamp_cra_cookie_bad=$?
+wamp_cra_cookie_static_file_bad=$?
 
 crossbar stop --cbdir=./cookie/.crossbar || true
+
+########################################################################################
+##
+## WAMP-CRA+Ticket static + database-store
+##
+rm -rf ./cookie/.crossbar/.cookies
+MYSECRET="secret123" crossbar start --cbdir=./cookie/.crossbar --config=config-database.json &
+sleep 10
+
+MYSECRET="secret123"     python ./cookie/client.py client1
+wamp_cra_cookie_static_db_good=$?
+
+MYSECRET="wrongpassword" python ./cookie/client.py client1
+wamp_cra_cookie_static_db_bad=$?
+
+crossbar stop --cbdir=./cookie/.crossbar || true
+
+########################################################################################
+##
+## WAMP-CRA+Ticket static + proxy-router + database-store
+##
+rm -rf ./cookie/.crossbar/.cookies
+MYSECRET="secret123" crossbar start --cbdir=./cookie/.crossbar --config=config-rtrpxy-database.json &
+sleep 15
+
+MYSECRET="secret123"     python ./cookie/client.py client1
+wamp_cra_cookie_rtrpxy_static_db_good=$?
+
+MYSECRET="wrongpassword" python ./cookie/client.py client1
+wamp_cra_cookie_rtrpxy_static_db_bad=$?
+
+crossbar stop --cbdir=./cookie/.crossbar || true
+
 
 ########################################################################################
 ##
@@ -27,5 +61,9 @@ echo ""
 
 exec >> test.log
 
-[ $wamp_cra_cookie_good   -eq 0 ] && echo "wamp-cra-cookie-good:                        OK" || echo "wamp-cra-cookie-good:                         FAIL"
-[ $wamp_cra_cookie_bad    -eq 1 ] && echo "wamp-cra-cookie-bad:                         OK" || echo "wamp-cra-cookie-bad:                          FAIL"
+[ $wamp_cra_cookie_static_file_good      -eq 0 ] && echo "wamp-cra-cookie-static-file-good:            OK" || echo "wamp-cra-cookie-static-file-good:            FAIL"
+[ $wamp_cra_cookie_static_file_bad       -eq 1 ] && echo "wamp-cra-cookie-static-file-bad:             OK" || echo "wamp-cra-cookie-static-file-bad:             FAIL"
+[ $wamp_cra_cookie_static_db_good        -eq 0 ] && echo "wamp-cra-cookie-static-db-good:              OK" || echo "wamp-cra-cookie-static-db-good:              FAIL"
+[ $wamp_cra_cookie_static_db_bad         -eq 1 ] && echo "wamp-cra-cookie-static-db-bad:               OK" || echo "wamp-cra-cookie-static-db-bad:               FAIL"
+[ $wamp_cra_cookie_rtrpxy_static_db_good -eq 0 ] && echo "wamp-cra-cookie-rtrpxy-static-db-good:       OK" || echo "wamp-cra-cookie-rtrpxy-static-db-good:       FAIL"
+[ $wamp_cra_cookie_rtrpxy_static_db_bad  -eq 1 ] && echo "wamp-cra-cookie-rtrpxy-static-db-bad:        OK" || echo "wamp-cra-cookie-rtrpxy-static-db-bad:        FAIL"
