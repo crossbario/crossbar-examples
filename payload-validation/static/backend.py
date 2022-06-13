08824db805ba4a4d8f9545b49f98e42752f6b890
Tobias Oberstein <tobias.oberstein@crossbario.com>
2022-06-13 18:53:51 +0200

    add payload validation example (#155)
    add payload validation example

---
+++ payload-validation/static/backend.py
@@ -0,0 +1,151 @@
+import os
+from pprint import pformat
+from typing import Optional
+
+from twisted.internet.defer import inlineCallbacks
+from twisted.internet.task import LoopingCall
+
+from txaio import time_ns
+from autobahn.util import hltype, hlval
+from autobahn import wamp
+from autobahn.wamp.exception import ApplicationError
+from autobahn.wamp.types import SessionDetails, CloseDetails, CallDetails, RegisterOptions, PublishOptions
+from autobahn.twisted import sleep
+from autobahn.twisted.wamp import ApplicationSession
+
+
+class ExampleBackend(ApplicationSession):
+
+    def __init__(self, config):
+        super().__init__(config)
+        self._message = 'Hello, world!'
+        self._counter = 0
+        self._periodic_loop = LoopingCall(self._periodic)
+
+    @inlineCallbacks
+    def onJoin(self, details: SessionDetails):
+        self.log.info('{func} Session joined with details {details}', func=hltype(self.onJoin), details=details)
+
+        regs = yield self.register(self)
+        for reg in regs:
+            self.log.info('{reg}', reg=reg)
+
+        if False:
+            self._periodic_loop.start(10.)
+
+        self.log.info('{func} Ready!', func=hltype(self.onJoin))
+
+    @inlineCallbacks
+    def _periodic(self):
+        self._counter += 1
+
+        evt = self._counter
+        try:
+            yield self.publish('eth.pydefi.clock.ba3b1e9f-3006-4eae-ae88-cf5896b36342.on_clock_tick', evt,
+                               options=PublishOptions(acknowledge=True))
+        except Exception as e:
+            if isinstance(e, ApplicationError) and e.error == 'wamp.error.invalid_argument':
+                if 'invalid arg type' not in e.args[0]:
+                    raise RuntimeError('did not find expected error text in exception!')
+            else:
+                raise RuntimeError('unexpected exception raised!')
+        else:
+            raise RuntimeError('invalid publish did not raise!')
+
+        evt = {'counter': self._counter}
+        pub = yield self.publish('eth.pydefi.clock.ba3b1e9f-3006-4eae-ae88-cf5896b36342.on_clock_tick', evt,
+                                 options=PublishOptions(acknowledge=True))
+        self.log.info('{func} published (publication_id={publication_id})', func=hltype(self._periodic),
+                      publication_id=hlval(pub.id))
+
+    def onLeave(self, details: CloseDetails):
+        if self._periodic_loop:
+            if self._periodic_loop.running:
+                self._periodic_loop.stop()
+            self._periodic_loop = None
+
+    @wamp.register('com.example.backend.get_time')
+    def get_time(self) -> int:
+        return time_ns()
+
+    @wamp.register('com.example.backend.add2', check_types=True)
+    def add2(self, x: int, y: int) -> int:
+        result = x + y
+        self.log.info('{func}: {x} + {y} = {result}', func=hltype(self.add2),
+                      result=hlval(result), x=hlval(x), y=hlval(y))
+        return result
+
+    @inlineCallbacks
+    @wamp.register('com.example.backend.slow_square', check_types=True)
+    def slow_square(self, x: int, delay: float) -> int:
+        result = x * x
+        self.log.info('{func}(delay={delay}): {x}^2 = {result}', func=hltype(self.slow_square),
+                      delay=hlval(delay), result=hlval(result), x=hlval(x))
+        yield sleep(delay)
+        return result
+
+    @wamp.register('com.example.backend.get_message', options=RegisterOptions(details=True))
+    def get_message(self, details: Optional[CallDetails]) -> str:
+        self.log.info('{func}: details={details}', func=hltype(self.get_message),  details=details)
+        return self._message
+
+    @inlineCallbacks
+    @wamp.register('com.example.backend.set_message', check_types=True, options=RegisterOptions(details=True))
+    def set_message(self, message: str, details: Optional[CallDetails]) -> bool:
+        if not 10 < len(message) < 100:
+            raise ApplicationError('{}.silly_message'.format(self._prefix), 'Your message too silly!', 10, 100)
+
+        if message != self._message:
+            old_message = self._message
+            self._message = message
+            notification = {
+                'user': details.caller_authid,
+                'old': old_message,
+                'new': message,
+            }
+            yield self.publish('com.example.backend.on_message_changed',
+                               notification,
+                               options=PublishOptions(acknowledge=True))
+            self.log.info('{func}:\n{notification}', func=hltype(self.set_message), notification=pformat(notification))
+            return True
+        else:
+            return False
+
+    @wamp.register('eth.pydefi.clock.ba3b1e9f-3006-4eae-ae88-cf5896b36342.get_clock_address',
+                   check_types=True,
+                   options=RegisterOptions(details=True))
+    def get_clock_address(self, details: Optional[CallDetails]):
+        return '0x29B6c56497CA179e9AAFD739BeBded3f23768903'
+
+    @wamp.register('eth.pydefi.'
+                   'replica.ba3b1e9f-3006-4eae-ae88-cf5896b36342.'
+                   'book.a17f0b45-1ed2-4b1a-9a7d-c112e8cd5d9b.'
+                   'get_candle_history',
+                   check_types=True,
+                   options=RegisterOptions(details=True))
+    def get_candle_history(self, period_dur: int, start_ts: int, limit: Optional[int] = None, details: Optional[CallDetails] = None):
+        self.log.info('{func} period_dur={period_dur}, start_ts={start_ts}, limit={limit}, details={details}', func=hltype(self.get_candle_history),
+                      period_dur=hlval(period_dur), start_ts=hlval(start_ts), limit=hlval(limit), details=details)
+        # trading.Candle
+        candle = {
+            'period_dur': 10,
+            'start_ts': 1654900797173358641,
+            'market_oid': os.urandom(16),
+            'modified': 1654900797173358641,
+            'quantity': 1.,
+            'volume': 1.,
+            'price_open': 1.,
+            'price_close': 1.,
+            'price_min': 1.,
+            'price_max': 1.,
+            'price_avg': 1.,
+            'price_var': 1.,
+            'price_avg_volw': 1.,
+            'price_var_volw': 1.,
+        }
+        if 5 < period_dur < 10:
+            candle['foobar_invalid'] = 'something'
+        elif 0 < period_dur <= 5:
+            candle['price_open'] = 'something'
+
+        return candle
---
+++ payload-validation/static/client.py
@@ -0,0 +1,231 @@
+import sys
+import argparse
+import uuid
+from typing import Optional
+
+import txaio
+from txaio import time_ns
+
+txaio.use_twisted()
+
+from twisted.internet import reactor
+from twisted.internet.error import ReactorNotRunning
+from twisted.internet.defer import inlineCallbacks
+
+from autobahn.util import hltype, hlval
+from autobahn.wamp import cryptosign
+from autobahn.wamp.types import CloseDetails, EventDetails
+from autobahn.wamp.exception import ApplicationError
+from autobahn.twisted.util import sleep
+from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
+
+
+class ExampleClient(ApplicationSession):
+
+    def __init__(self, config=None):
+        self.log.info('{func} initializing component: {config}', func=hltype(self.__init__), config=config)
+        super().__init__(config)
+        try:
+            self._key = cryptosign.CryptosignKey.from_file(config.extra['key'])
+        except Exception as e:
+            self.log.error(
+                'could not load client private key: {log_failure}', log_failure=e)
+            raise
+        else:
+            self.log.info('client key loaded: {pubkey}', pubkey=hlval(self._key.public_key()))
+
+    def onConnect(self):
+        self.log.info('{func} connected to router', func=hltype(self.onConnect))
+        try:
+            authid = self.config.extra['authid']
+            authextra = {
+                'pubkey': self._key.public_key(),
+                'channel_binding': None,
+            }
+            self.join(self.config.realm,
+                      authmethods=['cryptosign'],
+                      # authid may bee None for WAMP-cryptosign!
+                      authid=authid,
+                      authextra=authextra)
+        except Exception as e:
+            self.log.error('could not join realm: {log_failure}', log_failure=e)
+            raise
+
+    def onChallenge(self, challenge):
+        self.log.info('{func} authentication challenge received: {challenge}',
+                      func=hltype(self.onChallenge), challenge=challenge)
+
+        signed_challenge = self._key.sign_challenge(self, challenge, channel_id_type=None)
+
+        return signed_challenge
+
+    @inlineCallbacks
+    def onJoin(self, session_details):
+        self.log.info('{func} session joined:\n{details}', func=hltype(self.onJoin), details=session_details)
+
+        for i in range(5):
+            value = yield self.call('com.example.backend.get_time')
+            self.log.info('get_time(): {value}', value=hlval(value))
+            yield sleep(.5)
+
+        value = yield self.call('com.example.backend.add2', 23, 666)
+        self.log.info('add2(23, 666): {value}', value=hlval(value))
+
+        t0 = txaio.time_ns()
+        value = yield self.call('com.example.backend.slow_square', 16, 0.5)
+        t1 = txaio.time_ns()
+        duration = (t1 - t0) / 10**6
+        self.log.info('slow_square(16): {value} in {duration} ms', value=hlval(value), duration=hlval(duration))
+
+        def on_message_changed(notification, event_details: Optional[EventDetails] = None):
+            self.log.info('{func}: {notification}\n{details}',
+                          func=hltype(on_message_changed),
+                          notification=notification,
+                          details=event_details)
+
+        yield self.subscribe(on_message_changed, 'com.example.backend.on_message_changed')
+
+        value = yield self.call('com.example.backend.set_message',
+                                'Der Räuber Hotzenplotz ist da! ({})'.format(t0))
+        self.log.info('set_message(): {value}', value=hlval(value))
+
+        # FIXME: wamp.error.invalid_argument: call result from procedure ...
+        # clock_oid = uuid.UUID('ba3b1e9f-3006-4eae-ae88-cf5896b36342')
+        # result = yield self.call('eth.pydefi.clock.{}.get_clock_address'.format(clock_oid))
+        # self.log.info('get_clock_address(): {result}', result=hlval(result))
+
+        replica_oid = uuid.UUID('ba3b1e9f-3006-4eae-ae88-cf5896b36342')
+        book_oid = uuid.UUID('a17f0b45-1ed2-4b1a-9a7d-c112e8cd5d9b')
+        period = {
+            'period_dur': 12,
+            'start_ts': txaio.time_ns(),
+            'limit': 10,
+        }
+        procedure = 'eth.pydefi.replica.{}.book.{}.get_candle_history'.format(replica_oid, book_oid)
+
+        if True:
+            # test valid call
+            result = yield self.call(procedure, 12, txaio.time_ns())
+            self.log.info('get_candle_history(): {result}', result=hlval(result))
+
+        if True:
+            # test valid call
+            result = yield self.call(procedure, 12, txaio.time_ns(), limit=100)
+            self.log.info('get_candle_history(): {result}', result=hlval(result))
+
+        if True:
+            # test call with invalid args length (too many)
+            try:
+                yield self.call(procedure, 12, txaio.time_ns(), 'invalid additional argument')
+            except Exception as e:
+                if isinstance(e, ApplicationError) and e.error == 'wamp.error.invalid_argument':
+                    if 'unexpected positional argument' not in e.args[0]:
+                        raise RuntimeError('did not find expected error text in exception!')
+                else:
+                    raise RuntimeError('unexpected exception raised!')
+            else:
+                raise RuntimeError('invalid call did not raise!')
+
+        if True:
+            # test call with invalid args type
+            try:
+                yield self.call(procedure, 'foo', 'bar')
+            except Exception as e:
+                if isinstance(e, ApplicationError) and e.error == 'wamp.error.invalid_argument':
+                    if 'invalid type' not in e.args[0]:
+                        raise RuntimeError('did not find expected error text in exception!')
+                else:
+                    raise RuntimeError('unexpected exception raised!')
+            else:
+                raise RuntimeError('invalid call did not raise!')
+
+        if True:
+            # test call with invalid kwargs present
+            try:
+                yield self.call(procedure, 12, txaio.time_ns(), foo=23)
+            except Exception as e:
+                if isinstance(e, ApplicationError) and e.error == 'wamp.error.invalid_argument':
+                    if 'unexpected keyword argument' not in e.args[0]:
+                        raise RuntimeError('did not find expected error text in exception!')
+                else:
+                    raise RuntimeError('unexpected exception raised!')
+            else:
+                raise RuntimeError('invalid call did not raise!')
+
+        if True:
+            # test call with invalid _result_ key present
+            try:
+                period['period_dur'] = 8
+                yield self.call(procedure, period)
+            except Exception as e:
+                if isinstance(e, ApplicationError) and e.error == 'wamp.error.invalid_argument':
+                    if 'invalid type' not in e.args[0]:
+                        raise RuntimeError('did not find expected error text in exception!')
+                else:
+                    raise RuntimeError('unexpected exception raised!')
+            else:
+                raise RuntimeError('invalid call did not raise!')
+
+        if True:
+            # test call with invalid _result_ type in (valid) key
+            try:
+                period['period_dur'] = 3
+                yield self.call(procedure, period)
+            except Exception as e:
+                if isinstance(e, ApplicationError) and e.error == 'wamp.error.invalid_argument':
+                    if 'invalid type' not in e.args[0]:
+                        raise RuntimeError('did not find expected error text in exception!')
+                else:
+                    raise RuntimeError('unexpected exception raised!')
+            else:
+                raise RuntimeError('invalid call did not raise!')
+
+        self.leave()
+
+    def onLeave(self, details: CloseDetails):
+        self.log.info('{func} session closed: {details}', func=hltype(self.onLeave), details=details)
+        self.config.extra['exit_details'] = details
+        self.disconnect()
+
+    def onDisconnect(self):
+        self.log.info('{func} connection to router closed', func=hltype(self.onDisconnect))
+        try:
+            reactor.stop()
+        except ReactorNotRunning:
+            pass
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', dest='debug', action='store_true',
+                        default=False, help='Enable logging at level "debug".')
+    parser.add_argument('--url', dest='url', type=str, default='ws://localhost:8080/ws',
+                        help='The router URL (default: ws://localhost:8080/ws).')
+    parser.add_argument('--realm', dest='realm', type=str, default=None,
+                        help='The realm to join. If not provided, let the router auto-choose the realm.')
+    parser.add_argument('--authid', dest='authid', type=str, default=None,
+                        help='The authid to connect under. If not provided, let the router auto-choose the authid.')
+    parser.add_argument('--key', dest='key', type=str, required=True,
+                        help='The private client key to use for authentication. A 32 bytes file containing the raw '
+                             'Ed25519 private key.')
+    options = parser.parse_args()
+
+    if options.debug:
+        txaio.start_logging(level='debug')
+    else:
+        txaio.start_logging(level='info')
+
+    extra = {
+        'authid': options.authid,
+        'key': options.key,
+        'exit_details': Optional[CloseDetails]
+    }
+    runner = ApplicationRunner(url=options.url, realm=options.realm, extra=extra)
+    runner.run(ExampleClient)
+
+    if isinstance(extra['exit_details'], CloseDetails) and extra['exit_details'].reason == 'wamp.close.normal':
+        sys.exit(0)
+    else:
+        print(extra['exit_details'])
+        sys.exit(1)
